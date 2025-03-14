cce387c281fee49c24f75b22e9f46dda15bf65f3
claudemarjean <marjean.8888@gmail.com>
2025-03-15 00:45:27 +0300

    ajout pages d'accueil

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./App.css";
+import Events from "./Events";
 
-const events = [
-  {
-    id: 1,
-    date: "Lorem Ipsum Dolor",
-    location: "Lorem Ipsum Dolor",
-    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 2,
-    date: "Lorem Ipsum Dolor",
-    location: "Sit Amet Consectetur",
-    details: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    id: 3,
-    date: "Lorem Ipsum Dolor",
-    location: "Adipiscing Elit",
-    details: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
-  },
-];
+function HomePage() {
+  return (
+    <div className="container">
+      <h1>Bienvenue</h1>
+      <div className="buttons">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="btn"
+        >
+          <Link to="/events">Voir les événements</Link>
+        </motion.button>
 
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="btn drive"
+          onClick={() => window.open("https://drive.google.com/drive/folders/1vpoWsed2dIRBUypa4Xua3UBb61SuVf33", "_blank")}
+        >
+          Accéder au Drive
+        </motion.button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="container">
-      <h1> Titre</h1>
-      <div className="event-grid">
-        {events.map((event) => (
-          <motion.div
-            key={event.id}
-            className="event-card"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            <h2>{event.date}</h2>
-            <p className="location">{event.location}</p>
-            <p className="details">{event.details}</p>
-          </motion.div>
-        ))}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events" element={<Events />} />
+      </Routes>
+    </Router>
   );
 }
 
